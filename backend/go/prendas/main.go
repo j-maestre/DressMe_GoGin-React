3436c1229dabb567f09b7c222900af76685b059d
j-maestre <xemaiestacio@gmail.com>
2021-04-02 20:44:29 +0200

    frontend-> trying to fix cors problems

--- backend/go/prendas/main.go
+++ backend/go/prendas/main.go
@@ -69,14 +69,15 @@ func MakeRoutes(r *gin.Engine) {
 
 		fmt.Printf("c.Request.Method \n")
 
+		// c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "*" ) //"Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With"
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "*")
 		c.Writer.Header().Set("Content-Type", "application/json")
 
 		if c.Request.Method == "OPTIONS" {
-			fmt.Println("OPTIONs??????????????")
+			fmt.Println("OPTIONS")
 			c.AbortWithStatus(200)
 		}
 		c.Next()
--- frontend/src/components/DM-visited/DM-visited.tsx
+++ frontend/src/components/DM-visited/DM-visited.tsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import image from "../../assets/img/muestra.png";
 import { AppContext } from "../../State";
 import { prendas_service } from "../../Environment";
+import axios from 'axios';
 // import { pinSharp, heartOutline, enterOutline } from 'ionicons/icons';
 
 import {
@@ -26,31 +27,38 @@ const DM_visited = (props: any) => {
   let headers = new Headers();
   headers.append('Content-Type', 'application/json');
   headers.append('Accept', 'application/json');
-  headers.append('Access-Control-Allow-Origin', 'http://localhost:3000');
+  headers.append('Access-Control-Allow-Origin', '*');
   headers.append('Access-Control-Allow-Credentials', 'true');
   // headers.append('GET', 'POST', 'OPTIONS');
 
   useEffect(() => {
-
-    fetch(prendas_service, {  
-      // mode: 'cors',
-      credentials: 'include',
-      method: 'GET',
+    axios.get(prendas_service, //proxy uri
+    {
       headers: headers
-    }).then(res => res.json()).then(
-        (result) => {
-          console.log("OLE LOS CARACOLES");
-          console.log(result)
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          console.log("ERROR")
-          console.log(error)
-          // setError(error);
-        }
-      )
+    }).then(function (response) {
+      console.log("OLE LOS CARACOLES")
+      console.log(response);
+    });
+
+    // fetch(prendas_service, {  
+    //   // mode: 'cors',
+    //   credentials: 'include',
+    //   method: 'GET',
+    //   headers: headers
+    // }).then(res => res.json()).then(
+    //     (result) => {
+    //       console.log("OLE LOS CARACOLES");
+    //       console.log(result)
+    //     },
+    //     // Note: it's important to handle errors here
+    //     // instead of a catch() block so that we don't swallow
+    //     // exceptions from actual bugs in components.
+    //     (error) => {
+    //       console.log("ERROR")
+    //       console.log(error)
+    //       // setError(error);
+    //     }
+    //   )
   }, [])
 
   return (
