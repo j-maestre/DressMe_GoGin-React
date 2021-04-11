8eb094d4ebd4cfd5c76e0208e36852c0d7c96adc
j-maestre <xemaiestacio@gmail.com>
2021-04-11 18:49:07 +0200

    frontend-> 3d viewer change to pure threejs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { Component } from "react";
 import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import Home from './pages/Home';
 import './App.css';
+import * as THREE from "three";
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -29,22 +30,75 @@ import WardRobe from './pages/WardRobe';
 import Login from './pages/Login';
 import Register from './pages/Register';
 
-const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <Menu/>
-      <IonRouterOutlet  id="main">
-        <Route path="/home" component={Home} exact={true} />
-        <Route path="/shop" component={Shop} exact={true} />
-        {/* <Route path="/offers" component={PageTwo} exact={true} /> */}
-        {/* <Route path="/contact" component={PageTwo} exact={true} /> */}
-        <Route path="/wardrobe" component={WardRobe} exact={true} />
-        <Route path="/login" component={Login} exact={true} />
-        <Route path="/register" component={Register} exact={true} />
-        <Route exact path="/" render={() => <Redirect to="/home" />} />
-      </IonRouterOutlet>
-    </IonReactRouter>
-  </IonApp>
-);
+class App extends Component {
+  componentDidMount() {
+    // === THREE.JS CODE START ===
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    var cube = new THREE.Mesh( geometry, material );
+    scene.add( cube );
+    camera.position.z = 5;
+    var animate = function () {
+      requestAnimationFrame( animate );
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+      renderer.render( scene, camera );
+    };
+    animate();
+    // === THREE.JS EXAMPLE CODE END ===
+  }
+  render() {
+    return (
+
+      <IonApp>
+        <IonReactRouter>
+          <Menu/>
+          <IonRouterOutlet  id="main">
+            <Route path="/home" component={Home} exact={true} />
+            <Route path="/shop" component={Shop} exact={true} />
+            {/* <Route path="/offers" component={PageTwo} exact={true} /> */}
+            {/* <Route path="/contact" component={PageTwo} exact={true} /> */}
+            <Route path="/wardrobe" component={WardRobe} exact={true} />
+            <Route path="/login" component={Login} exact={true} />
+            <Route path="/register" component={Register} exact={true} />
+            <Route exact path="/" render={() => <Redirect to="/home" />} />
+          </IonRouterOutlet>
+        </IonReactRouter>
+        {/* <div ref={ref => (this.mount = ref)} /> */}
+      </IonApp>
+      
+      
+    )
+  }
+}
+
+
+
+// const App: React.FC = () => {
+
+
+//   return(
+//     <IonApp>
+//         <IonReactRouter>
+//           <Menu/>
+//           <IonRouterOutlet  id="main">
+//             <Route path="/home" component={Home} exact={true} />
+//             <Route path="/shop" component={Shop} exact={true} />
+//             {/* <Route path="/offers" component={PageTwo} exact={true} /> */}
+//             {/* <Route path="/contact" component={PageTwo} exact={true} /> */}
+//             <Route path="/wardrobe" component={WardRobe} exact={true} />
+//             <Route path="/login" component={Login} exact={true} />
+//             <Route path="/register" component={Register} exact={true} />
+//             <Route exact path="/" render={() => <Redirect to="/home" />} />
+//           </IonRouterOutlet>
+//         </IonReactRouter>
+//       </IonApp>
+  
+//   )}
 
 export default App;
--- frontend/src/components/DM-3dview/DM-3dview.tsx
+++ frontend/src/components/DM-3dview/DM-3dview.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState, useContext} from 'react';
+import React, {useEffect, useState, useContext, Component} from 'react';
 import ReactDOM from "react-dom";
 import { Link } from "react-router-dom";
 import image from "../../assets/img/muestra.png";
@@ -10,6 +10,12 @@ import { IonContent } from '@ionic/react';
 // 3dmodel
 
 import {OBJModel} from 'react-3d-viewer';
+import * as THREE from "three";
+import model_viewer from "./DM-3dview";
+
+// import person from "../../../public/models/person.obj";
+
+// /home/xema/Proyecto_TFG/Dress_Me/DressMe/frontend/public/models/person.obj
 
 
 // import {
@@ -24,27 +30,77 @@ import {OBJModel} from 'react-3d-viewer';
 
 
 
-const DM_3dview = (props: any) => {
+class DM_3dview extends Component {
+  
+
+  // componentDidMount() {
+  //   // === THREE.JS CODE START ===
+  //   var scene = new THREE.Scene();
+  //   var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+  //   var renderer = new THREE.WebGLRenderer();
+  //   renderer.setSize( window.innerWidth, window.innerHeight );
+  //   document.getElementById("model3d").appendChild( renderer.domElement );
+  //   // document.body.appendChild( renderer.domElement );
 
 
+  //   var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+  //   var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+  //   var cube = new THREE.Mesh( geometry, material );
+  //   scene.add( cube );
+  //   camera.position.z = 5;
+  //   var animate = function () {
+  //     requestAnimationFrame( animate );
+  //     cube.rotation.x += 0.01;
+  //     cube.rotation.y += 0.01;
+  //     renderer.render( scene, camera );
+  //   };
+  //   animate();
+  //   // === THREE.JS EXAMPLE CODE END ===
+  // }
+  // model_viewer.init();
+
+  
+
+
+  render(){
+    console.log("A ver que tiene esto");
+    // console.log(model_viewer)
+    // model_viewer();
+
+    
+    
+    // console.log(model_viewer)
+    // model_viewer;
   return (
-    <div className="model-content">
-      <OBJModel 
-        width="400" height="400"  
-        position={{x:0,y:-10,z:0}} 
-        src="./person.obj"
-        // zoom = "-3"
-        onLoad={()=>{
-          //state.isLoading=false
-          console.log("OLE LOS CARACOLES")
-        }}
-        onProgress={xhr=>{
-          //state.isLoading = true -> en un useState
-          console.log("Cargando modelo 3d...")
-        }}
-      />
+    <div id="model3d">
+
+    <div className="loading" id="js-loader"><div className="loader"></div></div>
+  
+    <div className="wrapper">
+        {/* <!-- The canvas element is used to draw the 3D scene --> */}
+    <canvas id="c"></canvas>
+
     </div>
-  );
+      <a target="_blank" href="https://tympanus.net/codrops/2019/10/14/how-to-create-an-interactive-3d-character-with-three-js/" className="tutorial-link">Follow the tutorial on Codrops</a>
+    
+      </div>
+    // <div className="model-content">
+    //   <OBJModel 
+    //     width="400" height="400"  
+    //     position={{x:0,y:-10,z:0}} 
+    //     src="./models/person.obj"
+    //     // zoom = "-3"
+    //     onLoad={()=>{
+    //       //state.isLoading=false
+    //       console.log("OLE LOS CARACOLES")
+    //     }}
+    //     onProgress={xhr=>{
+    //       //state.isLoading = true -> en un useState
+    //       console.log("Cargando modelo 3d...")
+    //     }}
+    //   />
+    // </div>
+  )};
 
 }
 
